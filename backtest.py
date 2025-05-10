IMPORTANT! revised portfolio cum return calculation, when prev_price is negative, original calculation method would go wrong

backtest.py
@@ -169,16 +169,20 @@ class PairTradingBacktester_TrainTest:
                 ret_list.append(0.0)  # No return when flat
             elif pos == 1:  # Long position
                 cur = row["spread"]
-                ret = cur / prev_price  # Return = current spread / entry spread
-                port_val *= ret  # Update portfolio value
+                # ret = cur / prev_price  # Return = current spread / entry spread
+                # port_val *= ret  # Update portfolio value
+                ret = (cur - prev_price) / np.abs(prev_price)
+                port_val *= (1+ret)
                 prev_price = cur  # Update previous price
                 ret_list.append(ret - 1.0)  # Daily return
                 if row["unwind_signal"]:
                     pos = 0  # Exit position on unwind signal
             elif pos == -1:  # Short position
                 cur = row["spread"]
-                ret = 2.0 - cur / prev_price  # Short return = 2 - (current / entry)
-                port_val *= ret  # Update portfolio value
+                # ret = 2.0 - cur / prev_price  # Short return = 2 - (current / entry)
+                # port_val *= ret  # Update portfolio value
+                ret = (prev_price - cur) / np.abs(prev_price)
+                port_val *= (1+ret)
                 prev_price = cur  # Update previous price
                 ret_list.append(ret - 1.0)  # Daily return
                 if row["unwind_signal"]:

backtest_MRP.py
@@ -67,8 +67,8 @@ class backtest_MRP:
             # current position is long
             elif position == 1:
                 curr_price = row['portfolio_value']
-                ret = curr_price / prev_price
-                portfolio_value = portfolio_value * ret
+                ret = (curr_price - prev_price) / np.abs(prev_price)
+                portfolio_value = portfolio_value * (ret + 1)
 
                 if row["unwind_signal"] == 1:
                     position = 0
@@ -76,8 +76,8 @@ class backtest_MRP:
             # current position is short
             elif position == -1:
                 curr_price = row['portfolio_value']
-                ret = 2 - curr_price/prev_price
-                portfolio_value = portfolio_value * ret
+                ret = (prev_price - curr_price) / np.abs(prev_price)
+                portfolio_value = portfolio_value * (ret + 1)
 
                 if row["unwind_signal"] == 1:
                     position = 0